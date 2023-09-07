8ee64036817c21c2e8c753b818319feeee464675
Rupinderjeet Dhanoa <rdhanoa05@outlook.com>
2023-09-07 23:45:26 +0100

    update + 1

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+
+'''
 class person:
     def __init__(self, name, age):
         self.name = name
@@ -7,5 +9,17 @@ person1 = person("John", 36)
 person2 = person("rupi", 17)
 print(person2.name)
 print(person2.age)
+'''
 
-        
+
+
+
+
+
+x = 4
+y = 5
+z = 7
+
+print( (lambda x: (x+y*z))(4))
+print(x+(y*z))
+print((x+y)*z)
